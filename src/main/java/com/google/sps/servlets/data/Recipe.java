995cd28480c2c54a61606062619b9f78b60027eb
GROD8962 <47120200+GROD8962@users.noreply.github.com>
2021-07-11 22:53:27 -0400

    Create Recipe data class

---
+++ src/main/java/com/google/sps/servlets/data/Recipe.java
@@ -0,0 +1,19 @@
+package com.google.sps.data;
+
+/** A recipe and its attributes. */
+public final class Recipe {
+
+  private final int id;
+  private final String title;
+  private final String link;
+  private final int likes;
+  private final String category;
+
+  public Task(int id, String title, String link, int likes, String category) {
+    this.id = id;
+    this.title = title;
+    this.link = link;
+    this.likes = likes;
+    this.category = category;
+  }
+}
