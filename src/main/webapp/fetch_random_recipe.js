30a9832d9a6d5a86d87d7d44c259738ea1fdb3b6
Tyler Jedziniak <tyjedz@gmail.com>
2021-07-16 21:58:54 +0000

    add links to recipe from the category page

--- src/main/webapp/fetch_random_recipe.js
+++ src/main/webapp/fetch_random_recipe.js
@@ -30,38 +30,14 @@ const jsonrecipes = [
     category: 'dinner'
     }
 ]
-/**
- * Finds the recipe of the given title
- * 
- * @param {string} title title of the recipe
- */
-function search(title) {
-  let recipe = NaN;
-  for (let i = 0; i < jsonrecipes.length; i++) {
-    if (jsonrecipes[i].title == title) {
-      recipe = jsonrecipes[i];
-      break;
-    }
-  }
-  if (recipe == NaN) {
-      this.console.log("title not in recipe list");
-  }
-  return recipe;
-}
-window.onload = function showRecipe(){
-  
-  const queryString = window.location.search;
-  const urlParams = new URLSearchParams(queryString);
-  const title = urlParams.get('title');
-  
-  let recipe = search(title);
-  this.console.log(recipe);
+function addRandomRecipe() {
+
+  // Pick a random recipe.
+  const jsonrecipe = jsonrecipes[Math.floor(Math.random() * jsonrecipes.length)];
+  const recipe = '\n \n Link: '+ jsonrecipe.link +
+                 '\n \n Title: ' + jsonrecipe.title +
+                 ' \n \n Likes: '+ jsonrecipe.likes +'\n \n Category: '+ jsonrecipe.category;
   // Add it to the page.
-  // const categoryContainer = document.getElementById('category-container');
-  const recipeInfo = '\n \n Link: '+ recipe.link +
-                 ' \n \n Likes: '+ recipe.likes +
-                 '\n \n Category: '+ recipe.category;
   const recipeContainer = document.getElementById('recipe-container');
-  recipeContainer.innerText = recipeInfo;
-  this.document.getElementById('recipe-title').innerText = recipe.title;
+  recipeContainer.innerText = recipe;
 }
