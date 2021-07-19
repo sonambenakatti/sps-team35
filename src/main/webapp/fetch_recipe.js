712790a1c23787f0b88089c4d0836637427a1c42
GROD8962 <47120200+GROD8962@users.noreply.github.com>
2021-07-19 14:44:31 -0400

    Update for parity with main

--- src/main/webapp/fetch_recipe.js
+++ src/main/webapp/fetch_recipe.js
@@ -1,13 +1,42 @@
+const jsonrecipes = [
+    {link:'https://www.allrecipes.com/recipe/74363/peanut-butter-popcorn/',
+    title:'Peanut Butter Popcorn',
+    likes: 0,
+    category: 'lunch'
+    },
+    {link:'https://www.allrecipes.com/recipe/25432/simple-country-ribs/',
+    title:'Simple Country Ribs ',
+    likes: 0,
+    category: 'dinner'
+    },
+    {link:'https://www.allrecipes.com/recipe/270860/garlic-naan/',
+    title:'Garlic-Naan',
+    likes: 0,
+    category: 'lunch'
+    },
+    {link:'https://www.allrecipes.com/recipe/95817/vanilla-crepes/',
+    title:'Vanilla Crepes',
+    likes: 0,
+    category: 'breakfast'
+    },
+    {link:'https://www.allrecipes.com/recipe/12130/peanut-butter-pie-i/',
+    title:'Peanut Butter Pie',
+    likes: 0,
+    category: 'dinner'
+    },
+    {link:'https://www.allrecipes.com/recipe/262533/chicken-apple-and-brussels-sprout-sheet-pan-dinner/',
+    title:'Chicken, Apple, and Brussels Sprout Sheet Pan Dinner',
+    likes: 0,
+    category: 'dinner'
+    }
+]
 function addRandomRecipe() {
-  
-const responseServer = await fetch('/list-recipes');
-    const resp = await responseServer.json();
-    const message = document.getElementById('datastore-recipes');
-    message.innerText = resp;
 
-  // Pick a random greeting.
-  const recipe = recipes[Math.floor(Math.random() * recipes.length)];
-
+  // Pick a random recipe.
+  const jsonrecipe = jsonrecipes[Math.floor(Math.random() * jsonrecipes.length)];
+  const recipe = '\n \n Link: '+ jsonrecipe.link +
+                 '\n \n Title: Peanut Butter Popcorn ' + jsonrecipe.title +
+                 ' \n \n Likes: '+ jsonrecipe.likes +'\n \n Category: '+ jsonrecipe.category;
   // Add it to the page.
   const recipeContainer = document.getElementById('recipe-container');
   recipeContainer.innerText = recipe;
